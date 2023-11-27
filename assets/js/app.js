aeef2df7737592a867acae4c81a76b5dee376e63
Brayan Hernandez <brayanh_03@hotmail.com>
2023-11-27 17:30:12 -0600

    Revision de codigo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,10 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
+const $n = document.querySelector('.name');//Se corrige, se pone punto para mencion de la clase
 const $b = document.querySelector('#blog');
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+async function displayUser(username) {//se pone async al principio
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
   console.log(data);
@@ -16,7 +16,7 @@ function displayUser(username) {
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}`;//Se agrega el ; y el $
 }
 
 displayUser('stolinski').catch(handleError);
